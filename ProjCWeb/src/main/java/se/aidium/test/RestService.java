835bc2a32206a6f868b637689a04990a757363b0
aidium <bjorn@aidium.com>
2013-12-26 19:20:19 +0100

    Fixed skinny war

--- ProjCWeb/src/main/java/se/aidium/test/RestService.java
+++ ProjCWeb/src/main/java/se/aidium/test/RestService.java
@@ -14,44 +14,39 @@ import javax.ws.rs.core.Response;
 
 @Path("orders")
 public class RestService {
-   // Stores state simply in a static collection class.
-   private static Map<String, String> orders = new TreeMap<String, String>();
+	// Stores state simply in a static collection class.
+	private static Map<String, String> orders = new TreeMap<String, String>();
 
-   @Path("/{order}")
-   @PUT
-   @Produces("text/html")
-   public String create(@PathParam("order") String order,
-                                    @QueryParam("customer_name") String customerName)
-   {
-      orders.put(order, customerName);
-      return "Added order #" + order;
-   }
+	@Path("/{order}")
+	@PUT
+	@Produces("text/html")
+	public String create(@PathParam("order") String order, @QueryParam("customer_name") String customerName) {
+		orders.put(order, customerName);
+		return "Added order #" + order;
+	}
 
-   @Path("/{order}")
-   @GET
-   @Produces("text/html")
-   public String find(@PathParam("order") String order)
-   {
-      if (orders.containsKey(order))
-         return "<h2>Details on Order #" + order +
-                    "</h2><p>Customer name: " + orders.get(order);
+	@Path("/{order}")
+	@GET
+	@Produces("text/html")
+	public String find(@PathParam("order") String order) {
+		if (orders.containsKey(order))
+			return "<h2>Details on Order #" + order + "</h2><p>Customer name: " + orders.get(order);
 
-      throw new WebApplicationException(Response.Status.NOT_FOUND);
-   }
+		throw new WebApplicationException(Response.Status.NOT_FOUND);
+	}
 
-   @Path("/list")
-   @GET
-   @Produces("text/html")
-   public String list()
-   {
-      String header = "<h2>All Orders</h2>\n";
+	@Path("/list")
+	@GET
+	@Produces("text/html")
+	public String list() {
+		String header = "<h2>All Orders</h2>\n";
 
-      header += "<ul>";
-      for (Map.Entry<String, String> order : orders.entrySet())
-         header += "\n<li>#" + order.getKey() + " for " + order.getValue() + "</li>";
+		header += "<ul>";
+		for (Map.Entry<String, String> order : orders.entrySet())
+			header += "\n<li>#" + order.getKey() + " for " + order.getValue() + "</li>";
 
-      header += "\n</ul>";
+		header += "\n</ul>";
 
-      return header;
-   }
+		return header;
+	}
 }
